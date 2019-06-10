4a13e20964fa4296f6982a9457728505725b63d9
Keiran King <me@keiranking.com>
2019-06-10 14:44:16 -0400

    working d3

--- src/assets/javascripts/index.js
+++ src/assets/javascripts/index.js
@@ -1,7 +1,74 @@
 import '../stylesheets/test.scss';
 import * as d3 from 'd3';
+// var d3Ez = require("d3-ez");
 
 const square = d3.selectAll("rect");
-square.style("fill", "orange");
+square.style("fill", "thistle");
+
+function randomDataset() {
+  var _randomNum = function() {
+    return Math.floor(Math.random() * 10);
+  };
+  var data = {
+    "key": "Languages",
+    "values": [{
+      "key": "JavaScript",
+      "value": _randomNum()
+    }, {
+      "key": "C++",
+      "value": _randomNum()
+    }, {
+      "key": "Java",
+      "value": _randomNum()
+    }, {
+      "key": "Ruby",
+      "value": _randomNum()
+    }, {
+      "key": "Python",
+      "value": _randomNum()
+    }, {
+      "key": "PHP",
+      "value": _randomNum()
+    }, {
+      "key": "Perl",
+      "value": _randomNum()
+    }, {
+      "key": "Basic",
+      "value": _randomNum()
+    }, {
+      "key": "Assembly",
+      "value": _randomNum()
+    }]
+  };
+  return data;
+}
+
+// var chart = d3.ez.chart.polarAreaChart().colors(d3.ez.palette.diverging(2));
+// var legend = d3.ez.component.legend().title('Top 10');
+// var title = d3.ez.component.title().mainText("Programming Languages").subText("Top 10 Languages Used By Developers");
+//
+// // Create chart base
+// var myChart = d3.ez.base()
+//   .width(750)
+//   .height(400)
+//   .chart(chart)
+//   .legend(legend)
+//   .title(title)
+//   .on("customValueMouseOver", function(d, i) {
+//     d3.select("#message").text(d.value);
+//   });
+//
+// // Add to page
+// function update() {
+//   var data = randomDataset();
+//   d3.select("#chartholder")
+//     .datum(data)
+//     .call(myChart);
+// }
+//
+// update();
+
+// const data = await d3.csv("file.csv");
+// console.log(data);
 
 console.log("Loaded index.js");
