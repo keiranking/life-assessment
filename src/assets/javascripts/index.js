b76ee1d27f03cfa318f579b3d396c0fb5fae0412
Keiran King <me@keiranking.com>
2019-06-11 20:27:06 -0400

    Add chart.js, polar area chart

--- src/assets/javascripts/index.js
+++ src/assets/javascripts/index.js
@@ -1,3 +1,27 @@
 import '../stylesheets/test.scss';
+import Chart from 'chart.js';
+
+let ctx = document.getElementById('myChart');
+
+let myChart = new Chart(ctx, {
+  type: 'polarArea',
+  options: {},
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 0
+    }]
+  }
+});
 
 console.log("Loaded index.js");
